185c814aa043f41353332504870985852abe54ff
ThomasR <thomasrutgers@gmail.com>
2019-11-05 09:42:19 +0100

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -2,18 +2,9 @@
 ServoKeyer servoKeyer(0,1);
 
 void setup() {
-  // put your setup code here, to run once:
-
-  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  servoKeyer.move(30,100,2000);
-
-  servoKeyer.move(105,45,2000);
-   servoKeyer.move(60,70,2000); 
-      servoKeyer.move(60,170,2000); 
-      servoKeyer.move(160,170,200); 
-          servoKeyer.move(60,170,200); 
+  servoKeyer.move(30,0,2000);
+  servoKeyer.move(105,0,2000);
 }
