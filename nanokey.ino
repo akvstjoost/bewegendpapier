c3940b0b52030795277d5e816030ac08f94f0860
ThomasR <thomasrutgers@gmail.com>
2020-10-14 10:05:34 +0200

    Update nanokey.ino

--- nanokey.ino
+++ nanokey.ino
@@ -3,35 +3,45 @@
 #include <Servo.h>
 Servo servo1;
 Servo servo2;
-int _pos1=0;
-int _pos2=0;
+int _pos1 = 0;
+int _pos2 = 0;
 
 void setup() {
   servo1.attach(2);
   servo2.attach(3);
+  pinMode(11, OUTPUT);
+  pinMode(12, INPUT_PULLUP);
+  digitalWrite(11, LOW);
 }
 
 void loop() {
   //schrijf hier je code
+  moveServos(0, 0, 1000);
+  moveServos(180, 180, 1000);
 
 }
 
-void moveServos(int pos1,int pos2,int time) {
+
+
+void moveServos(int pos1, int pos2, int time) {
   int loops = time / 20;
-  if (loops < 1) loops=1;
+  if (loops < 1) loops = 1;
   int start1 = _pos1;
   int start2 = _pos2;
   int stop1 = pos1;
   int stop2 = pos2;
-  for (int l=0;l<loops;l++) {
-    servo1.write(calcPos(l,loops,start1,stop1));
-    servo2.write(calcPos(l,loops,start2,stop2));
-    delay(20); 
+  for (int l = 0; l < loops; l++) {
+    servo1.write(calcPos(l, loops, start1, stop1));
+    servo2.write(calcPos(l, loops, start2, stop2));
+    delay(20);
+    do {
+    } while (digitalRead(12) == LOW);
+
   }
-    _pos1 = pos1;
-    _pos2 = pos2;
+  _pos1 = pos1;
+  _pos2 = pos2;
 }
 
 int calcPos(float loop, float loops, float start, float end) {
-  return int(end*(loop/loops))+ (start*(1-(loop/loops)));
+  return int(end * (loop / loops)) + (start * (1 - (loop / loops)));
 }
